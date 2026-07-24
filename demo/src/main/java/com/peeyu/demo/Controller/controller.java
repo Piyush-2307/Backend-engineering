e0d3ef3f0da3d5e44c81981b00a8a272a6c13dad
Peeyush Arora <peeyusharora831@gmail.com>
2026-07-24 23:52:09 +0530

    first commit

---
+++ demo/src/main/java/com/peeyu/demo/Controller/controller.java
@@ -0,0 +1,12 @@
+package com.peeyu.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @GetMapping()
+    public String hello(){
+        return "Hello";
+    }
+}
