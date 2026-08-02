adc0415cbbf712ec186bfdd15248315b1b1dbbad
Peeyush Arora <peeyusharora831@gmail.com>
2026-08-02 23:32:10 +0530

    3rd commit

--- demo/src/main/java/com/peeyu/demo/Controller/UserController.java
+++ demo/src/main/java/com/peeyu/demo/Controller/UserController.java
@@ -1,11 +1,13 @@
 package com.peeyu.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/users/")
 public class UserController {
-    @GetMapping("/")
+    @GetMapping
     public String hello(){
         return "Hello";
     }
